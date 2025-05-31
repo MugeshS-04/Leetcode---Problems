ca69c5115bdb7d271af49099a0f32d650d29cc35
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-06-01 01:59:38 +0530

    Time: 46 ms (5.35%), Space: 54.7 MB (99.05%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,48 +1,21 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int cursum = nums[0];
-        int curlen = 1;
-        int minlen = nums.length;
-        boolean mark = false;
-        int start = 0;
-        int end = 1;
+        int l = 0;
+        int cursum = 0;
+        int minlen = nums.length+1;
 
-        while(end < nums.length)
+        for(int r = 0; r < nums.length; r++)
         {
-            if(cursum < target)
+            cursum+=nums[r];
+
+            while(cursum >= target)
             {
-                cursum = cursum + nums[end++];
-                curlen++;
-            }
-            else
-            {
-               minlen = Math.min(minlen, curlen);
-               curlen--;
-               cursum = cursum-nums[start++]; 
-               mark = true;
+                System.out.println(l+" "+r);
+                minlen = Math.min(minlen, r-l+1);
+                cursum = cursum - nums[l];
+                l++;
             }
         }
-        
-        if(cursum >= target)
-        {
-            minlen = Math.min(curlen, minlen);
-            mark = true;
-        }
-
-        while(start < nums.length)
-        {
-            if(cursum - nums[start] >= target)
-            {
-                curlen--;
-                minlen = Math.min(curlen, minlen);
-                cursum-=nums[start++];
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        return minlen == nums.length && mark == false? 0 : minlen;
-    }
+        return minlen == nums.length+1 ? 0 : minlen;
+    }   
 }
