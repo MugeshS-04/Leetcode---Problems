6e15364bf6ecdd6f775749a3b2a85c7a7dd3d707
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-06-05 19:47:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int max = 0;
+        String result = s.charAt(0)+"";
+        for(int i = 0; i < s.length()-1; i++)
+        {
+            for(int j = i+1; j < s.length(); j++)
+            {
+                String sub = s.substring(i, j);
+                if(palindrome(sub, max) && j-i > max)
+                {
+                    max = j-i;
+                    result = new String(sub);
+                }
+            }
+        }
+        return result;
+    }
+
+    boolean palindrome(String sub, int max)
+    {
+        int start = 0;
+        int end = sub.length()-1;
+
+        if(end - start > max)
+        {
+            while(start < end)
+            {
+                if(sub.charAt(start++) != sub.charAt(end--))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
