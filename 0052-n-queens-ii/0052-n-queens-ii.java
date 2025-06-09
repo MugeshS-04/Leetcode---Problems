e2207396f0345b487fc8f618a8c8bb1a10b103dd
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-06-09 23:42:23 +0530

    Time: 1 ms (81.57%), Space: 40.1 MB (92.55%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int totalNQueens(int n) {
+        boolean[][] placed = new boolean[n][n];
+        int[] count = new int[1];
+        solve(placed, n, 0, count);
+        return count[0];
+    }
+
+    void solve(boolean[][] placed, int size, int row, int[] count)
+    {
+        if(row == size)
+        {
+            count[0]++;
+            return;
+        }
+
+        for(int col = 0; col < size; col++)
+        {
+            if(issafe(placed, size, row, col))
+            {
+                placed[row][col] = true;
+                solve(placed, size, row+1, count);
+                placed[row][col] = false;
+            }
+        }
+    }
+
+    boolean issafe(boolean[][] placed, int size, int row, int col)
+    {
+        for(int i = 0; i < size; i++)
+        {
+            if(placed[i][col])
+            {
+                return false;
+            }
+        }
+
+        for(int i = row-1, j = col - 1; i>=0 && j>=0; i--, j--)
+        {
+            if(placed[i][j])
+            {
+                return false;
+            }
+        }
+        for(int i = row-1, j = col+1; i>=0 && j<size; i--, j++)
+        {
+            if(placed[i][j])
+            {
+                return false;
+            }
+        }
+            
+        return true;
+    }
+}
