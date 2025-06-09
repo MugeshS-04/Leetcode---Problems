9bddcd86510aaa18f558b5e876885e4d885a8170
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-06-10 00:08:30 +0530

    Time: 374 ms (25.88%), Space: 41.3 MB (46.8%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,57 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    boolean solve(char[][] board)
+    {
+        for(int row = 0; row < 9; row++)
+        {
+            for(int col = 0; col < 9; col++)
+            {
+                if(board[row][col] == '.')
+                {
+                    for(char digit = '1'; digit <= '9'; digit++)
+                    {
+                        if(isvalid(board, row, col, digit))
+                        {
+                            board[row][col] = digit;
+                            if(solve(board))
+                            {
+                                return true;
+                            };
+                            board[row][col] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    boolean isvalid(char[][] board, int row, int col, char digit)
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            if(board[row][i] == digit)
+            {
+                return false;
+            }
+            if(board[i][col] == digit)
+            {
+                return false;
+            }
+
+            int rowgrid = 3 * (row/3) + i/3;
+            int colgrid = 3 * (col/3) + i%3;
+
+            if(board[rowgrid][colgrid] == digit)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
