0344fa25cd6040d88ffed4e78719724ccf26cda5
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-06-09 23:43:34 +0530

    Time: 2 ms (61.01%), Space: 41.9 MB (13.8%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,28 +1,56 @@
 class Solution {
     public int totalNQueens(int n) {
-        return this.helper(new boolean[n * 2], new boolean[n * 2], new boolean[n], 0, n);
+        boolean[][] placed = new boolean[n][n];
+        int[] count = new int[1];
+        solve(placed, n, 0, count);
+        System.gc();
+        return count[0];
     }
 
-    private int helper(final boolean[] diag1, final boolean[] diag2, final boolean[] col, final int row, final int n) {
-        if(row == n)
-            return 1;
+    void solve(boolean[][] placed, int size, int row, int[] count)
+    {
+        if(row == size)
+        {
+            count[0]++;
+            return;
+        }
 
-        int result = 0;
+        for(int col = 0; col < size; col++)
+        {
+            if(issafe(placed, size, row, col))
+            {
+                placed[row][col] = true;
+                solve(placed, size, row+1, count);
+                placed[row][col] = false;
+            }
+        }
+    }
 
-        for(int i = 0; i < n; ++i) {
-            if(!diag1[row + i] && !diag2[n - row + i] && !col[i]) {
-                diag1[row + i] = true;
-                diag2[n - row + i] = true;
-                col[i] = true;
-
-                result += this.helper(diag1, diag2, col, row + 1, n);
-
-                diag1[row + i] = false;
-                diag2[n - row + i] = false;
-                col[i] = false;
+    boolean issafe(boolean[][] placed, int size, int row, int col)
+    {
+        for(int i = 0; i < size; i++)
+        {
+            if(placed[i][col])
+            {
+                return false;
             }
         }
 
-        return result;
+        for(int i = row-1, j = col - 1; i>=0 && j>=0; i--, j--)
+        {
+            if(placed[i][j])
+            {
+                return false;
+            }
+        }
+        for(int i = row-1, j = col+1; i>=0 && j<size; i--, j++)
+        {
+            if(placed[i][j])
+            {
+                return false;
+            }
+        }
+            
+        return true;
     }
 }
