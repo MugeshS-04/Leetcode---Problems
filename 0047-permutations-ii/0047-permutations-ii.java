112e68a44b8ad6b97c4d8f2478cc40d856772767
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-06-17 00:38:57 +0530

    Time: 1 ms (99.91%), Space: 45.1 MB (50.49%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,29 +1,39 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> result = new ArrayList();
-        boolean[] used = new boolean[nums.length];
-        permutation(result, new ArrayList(), nums, used);
+        List<Integer> current = new ArrayList();
+        Arrays.sort(nums);
+        boolean used[] = new boolean[nums.length];
+        permute(result, current, nums, used);
         return result;
     }
 
-    void permutation(List<List<Integer>> result, List<Integer> current, int[] nums, boolean[] used)
+    public void permute(List<List<Integer>> result, List<Integer> current, int[] nums, boolean[] used)
     {
-        if(current.size() == nums.length && !result.contains(current))
+        if(current.size() == nums.length)
         {
             result.add(new ArrayList(current));
             return;
         }
 
+
         for(int i = 0; i < nums.length; i++)
         {
-            if(used[i] == false)
+            if(used[i])
             {
-                current.add(nums[i]);
-                used[i] = true;
-                permutation(result, current, nums, used);
-                current.remove(current.size() - 1);
-                used[i] = false;
+                continue;
             }
+
+            if(i > 0 && nums[i] == nums[i-1] && !used[i-1])
+            {
+                continue;
+            }
+
+            used[i] = true;
+            current.add(nums[i]);
+            permute(result, current, nums, used);
+            current.remove(current.size() - 1);
+            used[i] = false;
         }
     }
 }
