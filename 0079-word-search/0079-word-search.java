ea9c757e3cd2aaa1fde3d0ecb5f1bd7eebe288f8
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-06-22 02:26:33 +0530

    Time: 158 ms (38.41%), Space: 41.5 MB (88.92%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,41 +1,40 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        boolean[][] visited = new boolean[board.length][board[0].length];
+        boolean visited[][] = new boolean[board.length][board[0].length];
+
         for(int i = 0; i < board.length; i++)
         {
-            for(int j = 0; j < board[0].length; j++)
+            for(int j = 0; j < board[i].length; j++)
             {
-                if(wordsearch(board, visited, word, i, j, 0))
+                if(wordsearch(board, i, j, word, 0, visited))
                 {
                     return true;
                 }
             }
         }
-
         return false;
     }
 
-    boolean wordsearch(char[][] board, boolean[][] visited, String word, int row, int col, int iter)
+    boolean wordsearch(char[][] board, int row, int col, String word, int index, boolean visited[][])
     {
-        if(row < 0 || col < 0 || row >= board.length || col >= board[0].length || visited[row][col] || board[row][col] != word.charAt(iter))
+        if(row >= board.length || col >= board[0].length || row < 0 || col < 0 || index >= word.length() || board[row][col] != word.charAt(index) || visited[row][col])
         {
             return false;
         }
 
-        if(iter == word.length()-1)
+        visited[row][col] = true;
+
+        if(board[row][col] == word.charAt(index) && index == word.length()-1)
         {
             return true;
         }
 
-        visited[row][col] = true;
-
-        boolean found =  wordsearch(board, visited, word, row-1, col, iter + 1 ) ||
-                         wordsearch(board, visited, word, row+1, col, iter + 1) ||
-                         wordsearch(board, visited, word, row, col-1, iter + 1) ||
-                         wordsearch(board, visited, word, row, col+1, iter + 1);
+        boolean result = wordsearch(board, row+1, col, word, index+1, visited) ||
+                         wordsearch(board, row-1, col, word, index+1, visited) ||
+                         wordsearch(board, row, col+1, word, index+1, visited) ||
+                         wordsearch(board, row, col-1, word, index+1, visited);
         
-       visited[row][col] = false;
-
-       return found;
+        visited[row][col] = false;
+        return result;
     }
 }
