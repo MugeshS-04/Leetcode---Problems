ce685ed4dc742431099430f1076196878869dec6
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-06-30 22:57:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder sc = new StringBuilder();
+        String result = new String();
+        boolean flag = true;
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s.charAt(i) == ' ' && flag)
+            {
+                result = result + sc.toString();
+                sc.setLength(0);
+                flag = false;
+            }
+            else if(s.charAt(i) == ' ' )
+            {
+                result = result +" "+ sc.toString();
+                sc.setLength(0);
+            }
+            else
+            {
+                sc.insert(0, s.charAt(i));
+            }
+        }
+
+        result = result + " " + sc.toString();
+
+        return result;
+    }
+}
