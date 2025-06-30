92ab92fe2b55cbde2cd0959576b7fafaa1c754b5
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-06-30 23:11:23 +0530

    Time: 20 ms (14.42%), Space: 45.5 MB (31.38%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,31 +1,54 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder sc = new StringBuilder();
         String result = new String();
-        boolean flag = true;
+        int k = 0;
+        int i = 0;
 
-        for(int i = 0; i < s.length(); i++)
+        for(i = 0; i < s.length(); i++)
         {
-            if(s.charAt(i) == ' ' && flag)
+            if(s.charAt(i) == ' ' && k == 0)
             {
-                result = result + sc.toString();
-                sc.setLength(0);
-                flag = false;
+                String word = reverse(s.substring(k, i));
+                result += word;
+                k = i+1;
             }
-            else if(s.charAt(i) == ' ' )
+
+            else if(s.charAt(i) == ' ')
             {
-                result = result +" "+ sc.toString();
-                sc.setLength(0);
-            }
-            else
-            {
-                sc.insert(0, s.charAt(i));
+                String word = reverse(s.substring(k, i));
+                result += " " + word;
+                k = i+1;
             }
         }
 
-        if(!flag) result = result + " " + sc.toString();
-        else result = result +  sc.toString();
+        String word = reverse(s.substring(k, i));
+        if(k == 0) {
+            result += word;
+        }
+        else
+        {
+            result += " "+ word;
+        }
 
         return result;
+    }
+
+    String reverse(String word)
+    {
+        char[] w = word.toCharArray();
+        int start = 0;
+        int end = word.length() - 1;
+
+        while(start < end)
+        {
+            char temp = w[start];
+            w[start] = w[end];
+            w[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        return new String(w);
     }
 }
