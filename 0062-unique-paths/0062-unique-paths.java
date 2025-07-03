a0f7f21c35b6d000a1fa94cbe856d3ac11572b2c
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-07-04 00:26:05 +0530

    Time: 0 ms (100%), Space: 40.8 MB (16.63%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,6 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m+1][n+1];
+        int dp[][] = new int[m][n];
 
         for(int i = 0; i < m; i++)
         {
@@ -11,9 +11,9 @@ class Solution {
             dp[0][i] = 1;
         }
 
-        for(int i = 1; i <=m; i++)
+        for(int i = 1; i <m; i++)
         {
-            for(int j = 1; j <= n; j++)
+            for(int j = 1; j < n; j++)
             {
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
