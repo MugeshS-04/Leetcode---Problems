c41743fd620ddd2021e8a702a52c0c9420782d75
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-07-20 01:52:40 +0530

    Time: 7 ms (16.79%), Space: 52.6 MB (11.93%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -7,7 +7,7 @@ class Solution {
         {
             for(int j = 0; j < grid[0].length; j++)
             {
-                if(countIsland(grid, check, i, j, 0) != 0)
+                if(countIsland(grid, check, i, j, new int[1]) != 0)
                 {
                     count++;
                 }
@@ -17,17 +17,17 @@ class Solution {
         return count;
     }
 
-    int countIsland(char[][] grid, boolean[][] check, int row, int col, int val)
+    int countIsland(char[][] grid, boolean[][] check, int row, int col, int[] val)
     {
         if(row < 0 || row > grid.length-1 || col < 0 || col > grid[0].length-1 || check[row][col] == true || grid[row][col] == '0')
         {
-            return val;
+            return val[0];
         }
 
         if(grid[row][col] == '1')
         {
             check[row][col] = true;
-            val++;
+            val[0]++;
         }
 
         countIsland(grid, check, row+1, col, val);
@@ -35,7 +35,7 @@ class Solution {
         countIsland(grid, check, row, col+1, val);
         countIsland(grid, check, row, col-1, val);
 
-        return val;
+        return val[0];
 
         
     }
