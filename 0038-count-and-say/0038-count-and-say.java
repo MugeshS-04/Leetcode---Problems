ffe6b12c1485c68dd9cdb319f2d7c686b7b04903
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-07-29 17:32:42 +0530

    Time: 12 ms (19.48%), Space: 41.6 MB (49.02%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String countAndSay(int n) {
+        int count = 0;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append('1');
+        char val = '1';
+
+        for(int i = 1; i < n; i++)
+        {
+            StringBuilder temp = new StringBuilder();
+            val = sb.charAt(0);
+            count = 0;
+            
+            for(int j = 0; j < sb.length(); j++)
+            {
+                if(val == sb.charAt(j))
+                {
+                    count++;
+                    if(j+1 == sb.length())
+                    {
+                        temp.append(count);
+                        temp.append(val);
+
+                        System.out.println(temp);
+                    }
+                }
+                else
+                {
+                    temp.append(count);
+                    temp.append(val);
+                    val = sb.charAt(j);
+                    j--;
+                    count = 0;
+                }
+
+            }
+
+            sb.setLength(0);
+            sb.append(new String(temp));
+        }
+
+        return new String(sb);
+    }
+}
