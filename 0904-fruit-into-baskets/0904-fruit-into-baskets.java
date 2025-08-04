ed5d393b580ed979d91680f208ed4d3d370860ef
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-08-04 16:34:27 +0530

    Time: 18 ms (93.41%), Space: 55.3 MB (51.61%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int count = 0;
+        int fruit_count = 0;
+        int max = 0;
+
+        int end = 0;
+        int start = 0;
+
+        HashSet <Integer> myhash = new HashSet<>();
+
+        while(end < fruits.length)
+        {
+            if(!myhash.contains(fruits[end]))
+            {
+                if(fruit_count < 2)
+                {
+                    fruit_count++;
+                    count++;
+                    myhash.add(fruits[end]);
+                    end++;
+                }
+                else
+                {
+                    max = Math.max(max, count);
+                    fruit_count--;
+                    start = end - 1;
+                    count = 0;
+                    while(start >= 0 && fruits[start] == fruits[end - 1])
+                    {
+                        count++;
+                        start--;
+                    }
+                    myhash.remove(fruits[start]);
+                    start++;
+                }
+            }
+            else
+            {
+                count++;
+                end++;
+            }
+            max = Math.max(count, max);
+        }
+
+
+
+        return max;   
+    }
+}
