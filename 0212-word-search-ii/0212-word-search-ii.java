86d7f37582433554489434ec9a8ed1d01db3f044
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-08-16 20:52:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -0,0 +1,137 @@
+class Solution {
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> result = new ArrayList<>();
+        HashSet<String> myset = new HashSet<>();
+
+        boolean[][] check = new boolean[board.length][board[0].length];
+
+        Trie obj = new Trie();
+
+        for(String word : words)
+        {
+            obj.insert(word);
+        }
+
+        for(int i = 0; i < board.length; i++)
+        {
+            for(int j = 0; j < board[0].length; j++)
+            {
+                if(obj.startsWith(board[i][j]+""))
+                {
+                    search(board, i, j, result, myset, check, new StringBuilder(), obj);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    void search(char[][] board, int row, int col, List<String> result, HashSet<String> myset, boolean[][] check, StringBuilder sb, Trie obj)
+    {
+        if(row < 0 || col < 0 || 
+           row > board.length-1 || col > board[0].length-1 || 
+           check[row][col] == true || 
+           obj.startsWith(sb.toString()) == false)
+        {
+            return;
+        }
+
+        check[row][col] = true;
+
+        sb.append(board[row][col]);
+
+        if(obj.isword(sb.toString()) && !myset.contains(sb.toString()))
+        {
+            result.add(sb.toString());
+            myset.add(sb.toString());
+        }
+
+        search(board, row-1, col, result, myset, check, sb, obj);
+        search(board, row+1, col, result, myset, check, sb, obj);
+        search(board, row, col-1, result, myset, check, sb, obj);
+        search(board, row, col+1, result, myset, check, sb, obj);
+
+        check[row][col] = false;
+        sb.deleteCharAt(sb.length() - 1);
+
+        return;
+    }
+}
+
+class TrieNode {
+    TrieNode children[];
+    boolean isEnd;
+
+    TrieNode()
+    {
+        children = new TrieNode[26];
+        isEnd = false;
+    }
+    
+}
+
+class Trie {
+    TrieNode root;
+
+    Trie()
+    {
+        root = new TrieNode();
+    }
+
+    void insert(String word)
+    {
+        TrieNode node = root;
+
+        for(char ch : word.toCharArray())
+        {
+            int index = ch - 'a';
+
+            if(node.children[index] == null)
+            {
+                node.children[index] = new TrieNode();
+            }
+
+            node = node.children[index];
+        }
+
+        node.isEnd = true;
+    }
+
+    boolean startsWith(String word)
+    {
+        TrieNode node = root;
+
+        for(char ch : word.toCharArray())
+        {
+            int index = ch - 'a';
+            if(node.children[index] == null)
+            {
+                return false;
+            }
+            node = node.children[index];
+        }
+
+        return true;
+    }
+
+    boolean isword(String word)
+    {
+        TrieNode node = root;
+
+        for(char ch : word.toCharArray())
+        {
+            int index = ch - 'a';
+
+            if(node.children[index] == null)
+            {
+                return false;
+            }
+
+            node = node.children[index];
+        }
+
+        return node.isEnd;
+    }
+}
+
+
