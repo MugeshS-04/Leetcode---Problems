08526c4a790c3af01acea763394c35a34ecb0602
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-08-18 21:50:24 +0530

    Time: 5 ms (68.32%), Space: 63.2 MB (88.89%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode revnode = head;
+
+        while(fast != null && fast.next != null)
+        {
+            revnode = slow;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        revnode.next = reverse(slow);
+
+        ListNode start = head;
+        ListNode last = revnode.next;
+
+        int max = 0;
+
+        while(last != null)
+        {
+            int sum = start.val + last.val;
+            max = Math.max(max, sum);
+            start = start.next;
+            last = last.next;
+        }
+
+        return max;
+
+
+    }
+
+    ListNode reverse(ListNode head)
+    {
+        ListNode prev = null;
+        while(head != null)
+        {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+}
