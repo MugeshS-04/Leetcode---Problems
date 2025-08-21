a4f500cf2c00bbad60e0aeadcf94067458da7c35
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-08-21 19:43:48 +0530

    Time: 15 ms (77.03%), Space: 44.4 MB (73.45%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,8 +2,6 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int dp[] = new int[amount+1];
 
-        dp[0] = 0;
-
         for(int i = 1; i <= amount; i++)
         {
             dp[i] = amount+1;
@@ -11,11 +9,11 @@ class Solution {
             {
                 if(i - coin >= 0 && dp[i - coin] != amount+1)
                 {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                    dp[i] = Math.min(dp[i], dp[i - coin]+1);
                 }
             }
         }
 
-        return dp[amount] == amount + 1 ? -1 : dp[amount];
+        return dp[amount] == amount+1 ? -1 : dp[amount];
     }
 }
