aa299a7d83fb028680ffd62d33849d8f6e53904f
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-08-30 18:20:48 +0530

    Time: 3 ms (80.32%), Space: 44.6 MB (92.9%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,14 +1,5 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character, Integer> myhash = new HashMap<>();
-
-        myhash.put('I', 1);
-        myhash.put('V', 5);
-        myhash.put('X', 10);
-        myhash.put('L', 50);
-        myhash.put('C', 100);
-        myhash.put('D', 500);
-        myhash.put('M', 1000);
 
         int result = 0;
         
@@ -16,22 +7,48 @@ class Solution {
         {
             if(s.charAt(i) == 'I' && i+1 < s.length() && (s.charAt(i+1) == 'V' || s.charAt(i+1) == 'X'))
             {
-               result += myhash.get(s.charAt(i+1)) - 1;
-               i++;
+                if(s.charAt(i+1) == 'V') result += 4;
+                else result += 9;
+                i++;
             }
             else if(s.charAt(i) == 'X' && i+1 < s.length() && (s.charAt(i+1) == 'L' || s.charAt(i+1) == 'C'))
             {
-               result += myhash.get(s.charAt(i+1)) - 10;
-               i++;
+                if(s.charAt(i+1) == 'L') result += 40;
+                else result += 90;
+                i++;
             }
             else if(s.charAt(i) == 'C' && i+1 < s.length() && (s.charAt(i+1) == 'D' || s.charAt(i+1) == 'M'))
             {
-               result += myhash.get(s.charAt(i+1)) - 100;
-               i++;
+                if(s.charAt(i+1) == 'D') result += 400;
+                else result += 900;
+                i++;
             }
             else
             {
-                result += myhash.get(s.charAt(i));
+                switch(s.charAt(i))
+                {
+                    case 'I':
+                        result+=1;
+                        break;
+                    case 'V':
+                        result+=5;
+                        break;
+                    case 'X':
+                        result+=10;
+                        break;
+                    case 'L':
+                        result+=50;
+                        break;
+                    case 'C':
+                        result+=100;
+                        break;
+                    case 'D':
+                        result+=500;
+                        break;
+                    case 'M':
+                        result+=1000;
+                        break;
+                }
             }
         }
 
