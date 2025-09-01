9a2ea917f072d90b454c339defa6b939a9e20f33
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-09-01 20:26:33 +0530

    Time: 0 ms (100%), Space: 40.7 MB (30.84%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,24 +1,5 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        for(int i = 2; i <= n-2; i++)
-        {
-            String num = Integer.toString(n, i);
-            if(palindrome(num) != true) return false;
-        }
-        return true;
-    }
-
-    boolean palindrome(String num)
-    {
-        int start = 0;
-        int end = num.length()-1;
-
-        while(start < end)
-        {
-            if(num.charAt(start) != num.charAt(end)) return false;
-            start++;
-            end--;
-        }
-        return true;
+        return false;
     }
 }
