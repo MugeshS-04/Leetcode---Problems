deaccad8dcfddc7e74ee9f84c7212da8788a742b
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-09-29 16:16:52 +0530

    Time: 2 ms (72.25%), Space: 44.7 MB (87.11%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,14 +1,13 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> result = new ArrayList();
-        List<Integer> current = new ArrayList();
         Arrays.sort(nums);
-        boolean used[] = new boolean[nums.length];
-        permute(result, current, nums, used);
+        boolean[] used = new boolean[nums.length];
+        permutation(result, new ArrayList(), nums, used);
         return result;
     }
 
-    public void permute(List<List<Integer>> result, List<Integer> current, int[] nums, boolean[] used)
+    void permutation(List<List<Integer>> result, List<Integer> current, int[] nums, boolean[] used)
     {
         if(current.size() == nums.length)
         {
@@ -16,22 +15,15 @@ class Solution {
             return;
         }
 
-
         for(int i = 0; i < nums.length; i++)
         {
-            if(used[i])
+            if(used[i] || i > 0 && nums[i] == nums[i-1] && used[i-1] == false)
             {
                 continue;
             }
-
-            if(i > 0 && nums[i] == nums[i-1] && !used[i-1])
-            {
-                continue;
-            }
-
-            used[i] = true;
             current.add(nums[i]);
-            permute(result, current, nums, used);
+            used[i] = true;
+            permutation(result, current, nums, used);
             current.remove(current.size() - 1);
             used[i] = false;
         }
