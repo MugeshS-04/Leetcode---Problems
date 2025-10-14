107cfa9cdef806c5249d15b47c6fcdd299595bf7
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-10-14 20:19:51 +0530

    Time: 1 ms (100%), Space: 44.6 MB (50%) - LeetHub

---
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        if(k == 1 && nums.size() > 1) return true;
+
+        int start = 0;
+        int end = k;
+
+        while(end < nums.size())
+        {
+            if(check(nums, start, end))
+            {
+                if(check(nums, start + k, end + k)) return true;
+            }
+
+            start++;
+            end++;
+        }
+
+        return false;
+    }
+
+    boolean check(List<Integer> mylist, int start, int end)
+    {
+        for(int i = start+1; i < end; i++)
+        {
+            if(i >= mylist.size() || mylist.get(i) <= mylist.get(i-1))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
